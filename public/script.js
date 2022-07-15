4bd748cfc116a609a0adc23c7d9317c41481c6cd
Masashi SAKAI <masashi.sakai1986@gmail.com>
2022-07-15 17:35:35 +0900

    データを1つずつ取り出してHTMLに出力する

--- public/script.js
+++ public/script.js
@@ -1,9 +1,33 @@
+const tasksDOM = document.querySelector(".tasks");
+
 // /api/v1/tasksからタスクを読み込む
 const showTasks = async () => {
     try {
         // 自作のAPIを叩く
         const {data: tasks} = await axios.get("/api/v1/tasks");
-        console.log(tasks);
+
+        // タスクを出力する
+        const allTasks = tasks.map((task) => {
+            const { completed, _id, name } = task;
+            
+            return `<div class="single-task">
+            <h5>
+                <span><i class="fas fa-check-circle"></i></span>${name}
+            </h5>
+            <div class="task-links">
+                <!-- 編集リンク -->
+                <a href="#" class="edit-link">
+                    <i class="fas fa-edit"></i>
+                </a>
+                <!-- ゴミ箱リンク -->
+                <button type="button" class=""delete-btn">
+                    <i class="fas fa-trash"></i>
+                </button>
+            </div>
+        </div>`
+        })
+        .join("");
+        tasksDOM.innerHTML = allTasks;
     } catch (err) {
         console.log(err);
     }
